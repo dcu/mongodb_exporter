f52526ad06bebb2cb2c5f1a0363691b56cec0d30
Jorge Cuadrado <kuadrosxx@gmail.com>
2015-02-06 20:29:07 -0500

    Adds mgo dependency and configure mongo's session
    Signed-off-by: Jorge Cuadrado <kuadrosxx@gmail.com>

--- mongodb_exporter.go
+++ mongodb_exporter.go
@@ -3,10 +3,21 @@ package main
 import(
     "net/http"
     "github.com/prometheus/client_golang/prometheus"
+    "gopkg.in/mgo.v2"
+    "gopkg.in/mgo.v2/bson"
 )
 
 func main() {
+    session, err := mgo.Dial("localhost")
+    if err != nil {
+        panic(err)
+    }
+    defer session.Close()
+
+    session.SetMode(mgo.Eventual, true)
+    session.SetSocketTimeout(0)
+    defer session.Close()
+
     http.Handle("/metrics", prometheus.Handler())
     http.ListenAndServe(":9001", nil)
 }
-
