920b1299cb81b55133379b100629cdb2665016c7
David A. Cuadrado <krawek@gmail.com>
2015-02-06 20:17:31 -0500

    Implement the basic exporter.
    It is listening in the /metrics path and the 9001 port. The idea is
    making these parameters configurable in the future.

--- mongodb_exporter.go
+++ mongodb_exporter.go
@@ -1,5 +1,12 @@
 package main
 
+import(
+    "net/http"
+    "github.com/prometheus/client_golang/prometheus"
+)
+
 func main() {
+    http.Handle("/metrics", prometheus.Handler())
+    http.ListenAndServe(":9001", nil)
 }
 
