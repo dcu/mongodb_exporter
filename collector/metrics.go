0f6e374ef9ad701974504343c145abaa7c7d14ee
David A. Cuadrado <krawek@gmail.com>
2015-06-26 08:57:04 -0500

    Add more documentation.
    And also some checks to prevent crashes.
    
    Ref #3

--- collector/metrics.go
+++ collector/metrics.go
@@ -4,7 +4,7 @@ import (
 	"github.com/dcu/mongodb_exporter/shared"
 )
 
-// Metrics
+// DocumentStats are the stats associated to a document.
 type DocumentStats struct {
 	Deleted  float64 `bson:"deleted"`
 	Inserted float64 `bson:"inserted"`
@@ -12,6 +12,7 @@ type DocumentStats struct {
 	Updated  float64 `bson:"updated"`
 }
 
+// Export exposes the document stats to be consumed by the prometheus server.
 func (documentStats *DocumentStats) Export(groupName string) {
 	group := shared.FindOrCreateGroup(groupName)
 
@@ -21,11 +22,13 @@ func (documentStats *DocumentStats) Export(groupName string) {
 	group.Export("updated", documentStats.Updated)
 }
 
+// BenchmarkStats is bechmark info about an operation.
 type BenchmarkStats struct {
 	Num         float64 `bson:"num"`
 	TotalMillis float64 `bson:"totalMillis"`
 }
 
+// Export exports the benchmark stats.
 func (benchmarkStats *BenchmarkStats) Export(groupName string) {
 	group := shared.FindOrCreateGroup(groupName)
 
@@ -33,11 +36,13 @@ func (benchmarkStats *BenchmarkStats) Export(groupName string) {
 	group.Export("total_milliseconds", benchmarkStats.TotalMillis)
 }
 
+// GetLastErrorStats are the last error stats.
 type GetLastErrorStats struct {
 	Wtimeouts float64         `bson:"wtimeouts"`
 	Wtime     *BenchmarkStats `bson:"wtime"`
 }
 
+// Export exposes the get last error stats.
 func (getLastErrorStats *GetLastErrorStats) Export(groupName string) {
 	group := shared.FindOrCreateGroup(groupName)
 
@@ -45,12 +50,14 @@ func (getLastErrorStats *GetLastErrorStats) Export(groupName string) {
 	getLastErrorStats.Wtime.Export(groupName + "_wtime")
 }
 
+// OperationStats are the stats for some kind of operations.
 type OperationStats struct {
 	Fastmod      float64 `bson:"fastmod"`
 	Idhack       float64 `bson:"idhack"`
 	ScanAndOrder float64 `bson:"scanAndOrder"`
 }
 
+// Export exports the operation stats.
 func (operationStats *OperationStats) Export(groupName string) {
 	group := shared.FindOrCreateGroup(groupName)
 	group.Export("fastmod", operationStats.Fastmod)
@@ -58,31 +65,37 @@ func (operationStats *OperationStats) Export(groupName string) {
 	group.Export("scan_and_order", operationStats.ScanAndOrder)
 }
 
+// QueryExecutorStats are the stats associated with a query execution.
 type QueryExecutorStats struct {
 	Scanned        float64 `bson:"scanned"`
 	ScannedObjects float64 `bson:"scannedObjects"`
 }
 
+// Export exports the query executor stats.
 func (queryExecutorStats *QueryExecutorStats) Export(groupName string) {
 	group := shared.FindOrCreateGroup(groupName)
 	group.Export("scanned", queryExecutorStats.Scanned)
 	group.Export("scanned_objects", queryExecutorStats.ScannedObjects)
 }
 
+// RecordStats are stats associated with a record.
 type RecordStats struct {
 	Moves float64 `bson:"moves"`
 }
 
+// Export exposes the record stats.
 func (recordStats *RecordStats) Export(groupName string) {
 	group := shared.FindOrCreateGroup(groupName)
 	group.Export("moves_total", recordStats.Moves)
 }
 
+// ApplyStats are the stats associated with the apply operation.
 type ApplyStats struct {
 	Batches *BenchmarkStats `bson:"batches"`
 	Ops     float64         `bson:"ops"`
 }
 
+// Export exports the apply stats
 func (applyStats *ApplyStats) Export(groupName string) {
 	group := shared.FindOrCreateGroup(groupName)
 	group.Export("ops_total", applyStats.Ops)
@@ -90,12 +103,14 @@ func (applyStats *ApplyStats) Export(groupName string) {
 	applyStats.Batches.Export(groupName + "_batches")
 }
 
+// BufferStats are the stats associated with the buffer
 type BufferStats struct {
 	Count        float64 `bson:"count"`
 	MaxSizeBytes float64 `bson:"maxSizeBytes"`
 	SizeBytes    float64 `bson:"sizeBytes"`
 }
 
+// Export exports the buffer stats.
 func (bufferStats *BufferStats) Export(groupName string) {
 	group := shared.FindOrCreateGroup(groupName)
 	group.Export("count", bufferStats.Count)
@@ -103,6 +118,7 @@ func (bufferStats *BufferStats) Export(groupName string) {
 	group.Export("size_bytes", bufferStats.SizeBytes)
 }
 
+// MetricsNetworkStats are the network stats.
 type MetricsNetworkStats struct {
 	Bytes          float64         `bson:"bytes"`
 	Ops            float64         `bson:"ops"`
@@ -110,6 +126,7 @@ type MetricsNetworkStats struct {
 	ReadersCreated float64         `bson:"readersCreated"`
 }
 
+// Export exposes the network stats.
 func (metricsNetworkStats *MetricsNetworkStats) Export(groupName string) {
 	group := shared.FindOrCreateGroup(groupName)
 	group.Export("bytes_total", metricsNetworkStats.Bytes)
@@ -119,6 +136,7 @@ func (metricsNetworkStats *MetricsNetworkStats) Export(groupName string) {
 	metricsNetworkStats.GetMores.Export(groupName + "_getmores")
 }
 
+// ReplStats are the stats associated with the replication process.
 type ReplStats struct {
 	Apply        *ApplyStats          `bson:"apply"`
 	Buffer       *BufferStats         `bson:"buffer"`
@@ -126,6 +144,7 @@ type ReplStats struct {
 	PreloadStats *PreloadStats        `bson:"preload"`
 }
 
+// Export exposes the replication stats.
 func (replStats *ReplStats) Export(groupName string) {
 	replStats.Apply.Export(groupName + "_apply")
 	replStats.Buffer.Export(groupName + "_buffer")
@@ -133,22 +152,26 @@ func (replStats *ReplStats) Export(groupName string) {
 	replStats.PreloadStats.Export(groupName + "_preload")
 }
 
+// PreloadStats are the stats associated with preload operation.
 type PreloadStats struct {
 	Docs    *BenchmarkStats `bson:"docs"`
 	Indexes *BenchmarkStats `bson:"indexes"`
 }
 
+// Export exposes the preload stats.
 func (preloadStats *PreloadStats) Export(groupName string) {
 	preloadStats.Docs.Export(groupName + "_docs")
 	preloadStats.Indexes.Export(groupName + "_indexes")
 }
 
+// StorageStats are the stats associated with the storage.
 type StorageStats struct {
 	BucketExhausted float64 `bson:"freelist.search.bucketExhausted"`
 	Requests        float64 `bson:"freelist.search.requests"`
 	Scanned         float64 `bson:"freelist.search.scanned"`
 }
 
+// Export exports the storage stats.
 func (storageStats *StorageStats) Export(groupName string) {
 	group := shared.FindOrCreateGroup(groupName)
 
@@ -157,6 +180,7 @@ func (storageStats *StorageStats) Export(groupName string) {
 	group.Export("scanned", storageStats.Scanned)
 }
 
+// MetricsStats are all stats associated with metrics of the system
 type MetricsStats struct {
 	Document      *DocumentStats      `bson:"document"`
 	GetLastError  *GetLastErrorStats  `bson:"getLastError"`
@@ -167,12 +191,27 @@ type MetricsStats struct {
 	Storage       *StorageStats       `bson:"storage"`
 }
 
+// Export exports the metrics stats.
 func (metricsStats *MetricsStats) Export(groupName string) {
-	metricsStats.Document.Export(groupName + "_document_total")
-	metricsStats.GetLastError.Export(groupName + "_get_last_error")
-	metricsStats.Operation.Export(groupName + "_operation_total")
-	metricsStats.QueryExecutor.Export(groupName + "_query_executor_total")
-	metricsStats.Record.Export(groupName + "_record")
-	metricsStats.Repl.Export(groupName + "_repl")
-	metricsStats.Storage.Export(groupName + "_storage_freelist_search_total")
+	if metricsStats.Document != nil {
+		metricsStats.Document.Export(groupName + "_document_total")
+	}
+	if metricsStats.GetLastError != nil {
+		metricsStats.GetLastError.Export(groupName + "_get_last_error")
+	}
+	if metricsStats.Operation != nil {
+		metricsStats.Operation.Export(groupName + "_operation_total")
+	}
+	if metricsStats.QueryExecutor != nil {
+		metricsStats.QueryExecutor.Export(groupName + "_query_executor_total")
+	}
+	if metricsStats.Record != nil {
+		metricsStats.Record.Export(groupName + "_record")
+	}
+	if metricsStats.Repl != nil {
+		metricsStats.Repl.Export(groupName + "_repl")
+	}
+	if metricsStats.Storage != nil {
+		metricsStats.Storage.Export(groupName + "_storage_freelist_search_total")
+	}
 }
