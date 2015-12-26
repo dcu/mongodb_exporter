6e0411bb2413d157dd5a3cd67f7966df125d06a8
David A. Cuadrado <krawek@gmail.com>
2015-12-26 08:28:16 -0500

    yaml and bindata are no longer required.

--- Makefile
+++ Makefile
@@ -1,20 +1,14 @@
-test: bindata
+test:
 	go test github.com/dcu/mongodb_exporter/collector -cover -coverprofile=collector_coverage.out -short
 	go tool cover -func=collector_coverage.out
 	go test github.com/dcu/mongodb_exporter/shared -cover -coverprofile=shared_coverage.out -short
 	go tool cover -func=shared_coverage.out
 	@rm *.out
 
-bindata:
-	go-bindata -pkg=shared -o=shared/assets.go groups.yml
-
 deps:
 	go get -u github.com/prometheus/client_golang/prometheus
-	go get -u gopkg.in/yaml.v2
-	go get -u github.com/jteeuwen/go-bindata
 	go get -u gopkg.in/mgo.v2
 
-build: deps bindata
+build: deps
 	go build mongodb_exporter.go
-
 
--- mongodb_exporter.go
+++ mongodb_exporter.go
@@ -13,7 +13,7 @@ var (
 	listenAddressFlag = flag.String("web.listen-address", ":9001", "Address on which to expose metrics and web interface.")
 	metricsPathFlag   = flag.String("web.metrics-path", "/metrics", "Path under which to expose metrics.")
 
-	mongodbUriFlag    = flag.String("mongodb.uri", "mongodb://localhost:27017", "Mongodb URI, format: [mongodb://][user:pass@]host1[:port1][,host2[:port2],...][/database][?options]")
+	mongodbURIFlag    = flag.String("mongodb.uri", "mongodb://localhost:27017", "Mongodb URI, format: [mongodb://][user:pass@]host1[:port1][,host2[:port2],...][/database][?options]")
 	enabledGroupsFlag = flag.String("groups.enabled", "asserts,durability,background_flushing,connections,extra_info,global_lock,index_counters,network,op_counters,op_counters_repl,memory,locks,metrics", "Comma-separated list of groups to use, for more info see: docs.mongodb.org/manual/reference/command/serverStatus/")
 	//printCollectors   = flag.Bool("collectors.print", false, "If true, print available collectors and exit.")
 	authUserFlag = flag.String("auth.user", "", "Username for basic auth.")
@@ -68,11 +68,10 @@ func startWebServer() {
 
 func main() {
 	flag.Parse()
-	shared.LoadGroupsDesc()
 	shared.ParseEnabledGroups(*enabledGroupsFlag)
 
 	mongodbCollector := collector.NewMongodbCollector(collector.MongodbCollectorOpts{
-		URI: *mongodbUriFlag,
+		URI: *mongodbURIFlag,
 	})
 	prometheus.MustRegister(mongodbCollector)
 
