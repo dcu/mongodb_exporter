2b8bdaa80f8c82b9e6abad56b886ddd05f01b808
Tim Vaillancourt <tim@elementspace.com>
2016-01-26 14:24:21 +0100

    Modified code to closer match layout with structs, more to do/test

--- collector/mongos/balancer_status.go
+++ collector/mongos/balancer_status.go
@@ -1,17 +1,26 @@
-package main
+package collector_mongos
 
 import (
     "time"
-    "fmt"
+    "github.com/golang/glog"
+    "github.com/prometheus/client_golang/prometheus"
     "gopkg.in/mgo.v2"
     "gopkg.in/mgo.v2/bson"
 )
 
-func GetBalancerEnabled(session *mgo.Session) (bool) {
+var (
+    balancerInfo = prometheus.NewCounterVec(prometheus.CounterOpts{
+            Namespace: Namespace,
+            Name:      "balancer_info",
+            Help:      "TBD",
+    }, []string{"type"})
+)
+
+func IsBalancerEnabled(session *mgo.Session) (bool) {
     var balancerConfig map[string]interface{}
     err := session.DB("config").C("settings").Find(bson.M{ "_id" : "balancer" }).Select(bson.M{ "_id" : 0 }).One(&balancerConfig)
     if err != nil {
-        panic(err)
+        glog.Error("Could not find balancer settings in 'config.settings'!")
     }
 
     balancerStopped := balancerConfig["stopped"].(bool)
@@ -24,7 +33,7 @@ func GetBalancerEnabled(session *mgo.Session) (bool) {
 func GetTotalShards(session *mgo.Session) (int) {
     shardCount, err := session.DB("config").C("shards").Find(bson.M{}).Count()
     if err != nil {
-        panic(err)
+        glog.Error("Could not find shard information in 'config.settings'!")
     }
     return shardCount
 }
@@ -32,7 +41,7 @@ func GetTotalShards(session *mgo.Session) (int) {
 func GetTotalChunks(session *mgo.Session) (int) {
     chunkCount, err := session.DB("config").C("chunks").Find(bson.M{}).Count()
     if err != nil {
-        panic(err)
+        glog.Error("Could not find chunk information in 'config.chunks'!")
     }
     return chunkCount
 }
@@ -40,7 +49,7 @@ func GetTotalChunks(session *mgo.Session) (int) {
 func GetTotalShardedDatabases(session *mgo.Session) (int) {
     dbCount, err := session.DB("config").C("databases").Find(bson.M{ "partitioned" : true }).Count()
     if err != nil {
-        panic(err)
+        glog.Error("Could not find database information in 'config.databases'!")
     }
     return dbCount
 }
@@ -48,7 +57,7 @@ func GetTotalShardedDatabases(session *mgo.Session) (int) {
 func GetTotalShardedCollections(session *mgo.Session) (int) {
     collCount, err := session.DB("config").C("collections").Find(bson.M{ "dropped" : false }).Count()
     if err != nil {
-        panic(err)
+        glog.Error("Could not find collection information in 'config.collections'!")
     }
     return collCount
 }
@@ -61,7 +70,7 @@ func GetBalancerRoundCount24h(errorOccured bool, session *mgo.Session) (int) {
     findQuery := bson.M{ "what" : "balancer.round", "details.errorOccured" : errorOccured, "time" : bson.M{ "$gt" : TwentyFourHoursAgo() } }
     roundCount, err := session.DB("config").C("actionlog").Find(findQuery).Count()
     if err != nil {
-        panic(err)
+        glog.Error("Could not find balancer round info in 'config.actionlog'!")
     }
     return roundCount
 }
@@ -70,15 +79,11 @@ func GetFailedBalancerRoundCount24h(session *mgo.Session) (int) {
     return GetBalancerRoundCount24h(true, session)
 }
 
-func GetOkBalancerRoundCount24h(session *mgo.Session) (int) {
-    return GetBalancerRoundCount24h(false, session)
-}
-
 func GetSplitCount24h(session *mgo.Session) (int) {
     findQuery := bson.M{ "what" : "split", "time" : bson.M{ "$gt" : TwentyFourHoursAgo() } }
     splitCount, err := session.DB("config").C("changelog").Find(findQuery).Count()
     if err != nil {
-        panic(err)
+        glog.Error("Could not find split information in 'config.changelog'!")
     }
     return splitCount
 }
@@ -87,37 +92,27 @@ func GetMoveChunkStartCount24h(session *mgo.Session) (int) {
     findQuery := bson.M{ "what" : "moveChunk.start", "time" : bson.M{ "$gt" : TwentyFourHoursAgo() } }
     moveChunkStartCount, err := session.DB("config").C("changelog").Find(findQuery).Count()
     if err != nil {
-        panic(err)
+        glog.Error("Could not find moveChunk.start info in 'config.changelog'!")
     }
     return moveChunkStartCount
 }
 
-type ShardChunkInfo struct {
-    Count int
-    PercentTot float64
-}
-
-func GetAllShardChunkInfo(session *mgo.Session) (map[string]ShardChunkInfo) {
-    var result []map[string]interface{}
+func GetAllShardChunkInfo(session *mgo.Session) (map[string]int) {
     err := session.DB("config").C("chunks").Pipe([]bson.M{{ "$group" : bson.M{ "_id" : "$shard", "count" : bson.M{ "$sum" : 1  } } }}).All(&result)
     if err != nil {
-        panic(err)
+        glog.Error("Could not find shard chunk info!")
     }
 
-    totalChunks := GetTotalChunks(session)
-    shardChunkCounts := make(map[string]ShardChunkInfo)
+    shardChunkCounts := make(map[string]int)
     for _, element := range result {
         shard := element["_id"].(string)
-        count := element["count"].(int)
-        ptot := float64(count) / float64(totalChunks) * 100
-        shardChunkCounts[shard] = ShardChunkInfo{ Count: count, PercentTot: ptot }
+        shardChunkCounts[shard] = element["count"].(int)
     }
 
     return shardChunkCounts
 }
 
 func IsClusterBalanced(session *mgo.Session) (bool) {
-
     // Different thresholds based on size
     // http://docs.mongodb.org/manual/core/sharding-internals/#sharding-migration-thresholds
     var threshold int
@@ -133,8 +128,7 @@ func IsClusterBalanced(session *mgo.Session) (bool) {
     var minChunkCount int = -1
     var maxChunkCount int = 0
     shardChunkInfoAll := GetAllShardChunkInfo(session)
-    for _, shardChunkInfo := range shardChunkInfoAll {
-        chunkCount := shardChunkInfo.Count
+    for _, chunkCount := range shardChunkInfoAll {
         if chunkCount > maxChunkCount {
             maxChunkCount = chunkCount
         } else {
@@ -155,22 +149,46 @@ func IsClusterBalanced(session *mgo.Session) (bool) {
     return false
 }
 
-func main() {
-    uri := "mongodb://localhost:27018"
-    session, err := mgo.Dial(uri)
-    if err != nil {
-        fmt.Println("Failed to get collection stats.")
-    }
+type BalancerStatus struct {
+    IsBalanced			bool	
+    BalancerEnabled		bool
+    TotalShards			int
+    TotalChunks			int
+    TotalDatabases		int
+    TotalCollections		int
+    BalancerRoundFailureLast24h	int
+    SplitCountLast24h		int
+    MoveChunkStartCountLast24h	int
+}
 
-    fmt.Println("balancer enabled:", GetBalancerEnabled(session))
-    fmt.Println("cluster balanced:", IsClusterBalanced(session))
-    fmt.Println("total shards:", GetTotalShards(session))
-    fmt.Println("total chunks:", GetTotalChunks(session))
-    fmt.Println("total sharding-enabled dbs:", GetTotalShardedDatabases(session))
-    fmt.Println("total sharded collections:", GetTotalShardedCollections(session))
-    fmt.Println("failed balancer rounds in last 24hrs:", GetFailedBalancerRoundCount24h(session))
-    fmt.Println("moveChunk.start ops in last 24hrs:", GetMoveChunkStartCount24h(session))
-    fmt.Println("chunk splits in last 24hrs:", GetSplitCount24h(session))
+func (status *BalancerStatus) Export(ch chan<- prometheus.Metric) {
+    balancerInfo.WithLabelValues("is_balanced").Set(status.IsBalanced)
+    balancerInfo.WithLabelValues("balancer_on").Set(status.BalancerEnabled)
+    balancerInfo.WithLabelValues("total_shards").Set(status.TotalShards)
+    balancerInfo.WithLabelValues("total_chunks").Set(status.TotalChunks)
+    balancerInfo.WithLabelValues("total_dbs").Set(status.TotalDatabases)
+    balancerInfo.WithLabelValues("total_cols").Set(status.TotalCollections)
+    balancerInfo.WithLabelValues("failed_balances_24h").Set(status.BalancerRoundFailureLast24h)
+    balancerInfo.WithLabelValues("splits_24h").Set(status.SplitCountLast24h)
+    balancerInfo.WithLabelValues("move_chunk_starts_24h").Set(status.MoveChunkStartCountLast24h)
+    balancerInfo.Collect(ch)
+}
 
-    session.Close()
+func GetBalancerStatus(session *.mgo.Session) *BalancerStatus {
+    results := &BalancerStatus{}
+
+    session.SetMode(mgo.Eventual, true)
+    session.SetSocketTimeout(0)
+
+    results.IsBalanced = IsClusterBalanced(session)
+    results.BalancerEnabled = IsBalancerEnabled(session)
+    results.TotalShards = GetTotalShards(session)
+    results.TotalChunks = GetTotalChunks(session)
+    results.TotalDatabases = GetTotalShardedDatabases(session)
+    results.TotalCollections = GetTotalShardedCollections(session)
+    results.BalancerRoundFailureLast24h = GetFailedBalancerRoundCount24h(session)
+    results.SplitCountLast24h = GetSplitCount24h(session)
+    results.MoveChunkStartCountLast24h = GetMoveChunkStartCount24h(session)
+
+    return results
 }
