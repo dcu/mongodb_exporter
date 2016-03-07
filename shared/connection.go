1882d0d4bf61bb956cc6a1c4ee4dc1544c99f80d
David A. Cuadrado <krawek@gmail.com>
2016-03-07 09:51:44 -0500

    Force direct connection to mongodb.
    Fixes #12 #7

--- shared/connection.go
+++ shared/connection.go
@@ -1,17 +1,34 @@
 package shared
 
 import (
+	"time"
+
 	"github.com/golang/glog"
 	"gopkg.in/mgo.v2"
 )
 
+const (
+	dialMongodbTimeout = 10 * time.Second
+	syncMongodbTimeout = 1 * time.Minute
+)
+
 func MongoSession(uri string) *mgo.Session {
-	session, err := mgo.Dial(uri)
+	dialInfo, err := mgo.ParseURL(uri)
 	if err != nil {
-		glog.Errorf("Cannot connect to server using url: %s", uri)
+		glog.Errorf("Cannot connect to server using url %s: %s", uri, err)
+		return nil
+	}
+
+	dialInfo.Direct = true // Force direct connection
+	dialInfo.Timeout = dialMongodbTimeout
+
+	session, err := mgo.DialWithInfo(dialInfo)
+	if err != nil {
+		glog.Errorf("Cannot connect to server using url %s: %s", uri, err)
 		return nil
 	}
 	session.SetMode(mgo.Eventual, true)
+	session.SetSyncTimeout(syncMongodbTimeout)
 	session.SetSocketTimeout(0)
 	return session
 }
